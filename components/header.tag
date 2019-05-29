099bd2da758841fa2cba0c406b09cdb6442d0779
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 19:42:00 +0000

    Deploy YahArt/tasky to github.com/YahArt/tasky.git:gh-pages

--- components/header.tag
+++ components/header.tag
@@ -3,13 +3,9 @@
     <h1>Tasky</h1>
     <!-- Image is from: https://t2.rbxcdn.com/f37dbf676862a314cdb3f98c08460146-->
     <img src="./images/profile-picture.png" class="profile-picture rounded-circle" alt="Profile Picture">
-    <p>{this.currentUser.name}</p>
-    <p>Level {this.currentUser.level}</p>
-    <div class="progress">
-      <p class="progress-text">Punkte:
-      </p>
-      <div class="progress-bar progress-bar-striped progress-bar-animated progress-health" role="progressbar" aria-valuenow="{this.displayedExperiencePoints}" aria-valuemin="0" aria-valuemax="{this.experienceUntilNextLevel}" style="width: {this.displayedExperiencePoints}%">{this.displayedExperiencePoints}/{this.experienceUntilNextLevel}</div>
-    </div>
+    <p class="user-name">{this.currentUser.name}</p>
+    <p class="user-level">Level {this.currentUser.level}</p>
+    <div id="levelProgress" class="ldBar label-center" data-value="{this.percentage}" data-stroke="data:ldbar/res,stripe(#ff9,#fc9,1)" style="width:30%;height:30%;margin:auto"></div>
   </div>
 
 </div>
@@ -28,19 +24,13 @@
     width: 150px;
     height: auto;
   }
-  .progress {
-    width: 350px;
-    margin-bottom: 15px;
-  }
-  .progress-text {
+
+  .user-name {
     font-weight: bold;
-    margin-right: 15px;
   }
-  .progress-health {
-    background-color: #4CAF50;
-  }
-  .progress-magic {
-    background-color: #9C27B0;
+
+  .user-level {
+    font-size: 2rem;
   }
 </style>
 
@@ -51,19 +41,20 @@
     name: '',
     level: 1,
     experiencePoints: 50,
-    skills: [],
+    skills: []
   };
 
   this.currentUser = defaultUser;
-  this.experienceUntilNextLevel = defaultUser.level * 50;
+  this.experienceUntilNextLevel = defaultUser.level * 150;
   this.displayedExperiencePoints = defaultUser.experiencePoints;
+  this.percentage = 0;
   this.update();
   this.on('mount', function () {
+      this.progressElement = new ldBar('#levelProgress');
     this.userRepoGetAllUsers().then((users) => {
       this.currentUser = users[0];
-      this.calculateUserLevel();
       this.calculateExperiencePointsUntilNextLevel();
-      this.calculateDisplayedExperiencePoints();
+      this.calculatePercentage();
       this.update();
     });
   });
@@ -73,27 +64,30 @@
     this.currentUser.experiencePoints = this.currentUser.experiencePoints + points;
     this.calculateUserLevel();
     this.calculateExperiencePointsUntilNextLevel();
-    this.calculateDisplayedExperiencePoints();
+    this.calculatePercentage();
     this.update();
 
   });
 
   // TODO: Fix this stuff...
-  this.calculateExperiencePointsUntilNextLevel = function() {
+  this.calculateExperiencePointsUntilNextLevel = function () {
     this.experienceUntilNextLevel = this.currentUser.level * 70;
   }
 
-  this.calculateDisplayedExperiencePoints = function() {
-    this.displayedExperiencePoints = this.currentUser.experiencePoints % this.experienceUntilNextLevel;
-  }
-
-  this.calculateUserLevel = function() {
+  this.calculateUserLevel = function () {
     if (this.currentUser.experiencePoints >= this.experienceUntilNextLevel) {
-      this.currentUser.experiencePoints = this.currentUser.experiencePoints % this.experienceUntilNextLevel;
       this.currentUser.level = this.currentUser.level + 1;
+      // Start at the beginning again
+      this.currentUser.experiencePoints = 0;
     }
   }
 
+  this.calculatePercentage = function () {
+    this.percentage = (100 / this.experienceUntilNextLevel) * this.currentUser.experiencePoints;
+    if (this.progressElement) {
+      this.progressElement.set(this.percentage);
+    }
+  }
 </script>
 
 </header>
--- js/header.js
+++ js/header.js
@@ -1,23 +1,24 @@
-riot.tag2('header', '<div class="header"> <h1>Tasky</h1> <img src="./images/profile-picture.png" class="profile-picture rounded-circle" alt="Profile Picture"> <p>{this.currentUser.name}</p> <p>Level {this.currentUser.level}</p> <div class="progress"> <p class="progress-text">Punkte: </p> <div class="progress-bar progress-bar-striped progress-bar-animated progress-health" role="progressbar" aria-valuenow="{this.displayedExperiencePoints}" aria-valuemin="0" aria-valuemax="{this.experienceUntilNextLevel}" riot-style="width: {this.displayedExperiencePoints}%">{this.displayedExperiencePoints}/{this.experienceUntilNextLevel}</div> </div> </div> </div>', 'header .header,[data-is="header"] .header{ display: flex; flex-direction: column; justify-content: center; align-items: center; height: 400px; border-bottom: 1px solid black; } header .profile-picture,[data-is="header"] .profile-picture{ margin: 10px 0; width: 150px; height: auto; } header .progress,[data-is="header"] .progress{ width: 350px; margin-bottom: 15px; } header .progress-text,[data-is="header"] .progress-text{ font-weight: bold; margin-right: 15px; } header .progress-health,[data-is="header"] .progress-health{ background-color: #4CAF50; } header .progress-magic,[data-is="header"] .progress-magic{ background-color: #9C27B0; }', '', function(opts) {
+riot.tag2('header', '<div class="header"> <h1>Tasky</h1> <img src="./images/profile-picture.png" class="profile-picture rounded-circle" alt="Profile Picture"> <p class="user-name">{this.currentUser.name}</p> <p class="user-level">Level {this.currentUser.level}</p> <div id="levelProgress" class="ldBar label-center" data-value="{this.percentage}" data-stroke="data:ldbar/res,stripe(#ff9,#fc9,1)" style="width:30%;height:30%;margin:auto"></div> </div> </div>', 'header .header,[data-is="header"] .header{ display: flex; flex-direction: column; justify-content: center; align-items: center; height: 400px; border-bottom: 1px solid black; } header .profile-picture,[data-is="header"] .profile-picture{ margin: 10px 0; width: 150px; height: auto; } header .user-name,[data-is="header"] .user-name{ font-weight: bold; } header .user-level,[data-is="header"] .user-level{ font-size: 2rem; }', '', function(opts) {
   // Require tasky repository
   this.mixin('TaskyRepository');
   const defaultUser = {
     name: '',
     level: 1,
     experiencePoints: 50,
-    skills: [],
+    skills: []
   };
 
   this.currentUser = defaultUser;
-  this.experienceUntilNextLevel = defaultUser.level * 50;
+  this.experienceUntilNextLevel = defaultUser.level * 150;
   this.displayedExperiencePoints = defaultUser.experiencePoints;
+  this.percentage = 0;
   this.update();
   this.on('mount', function () {
+      this.progressElement = new ldBar('#levelProgress');
     this.userRepoGetAllUsers().then((users) => {
       this.currentUser = users[0];
-      this.calculateUserLevel();
       this.calculateExperiencePointsUntilNextLevel();
-      this.calculateDisplayedExperiencePoints();
+      this.calculatePercentage();
       this.update();
     });
   });
@@ -27,25 +28,28 @@ riot.tag2('header', '<div class="header"> <h1>Tasky</h1> <img src="./images/prof
     this.currentUser.experiencePoints = this.currentUser.experiencePoints + points;
     this.calculateUserLevel();
     this.calculateExperiencePointsUntilNextLevel();
-    this.calculateDisplayedExperiencePoints();
+    this.calculatePercentage();
     this.update();
 
   });
 
   // TODO: Fix this stuff...
-  this.calculateExperiencePointsUntilNextLevel = function() {
+  this.calculateExperiencePointsUntilNextLevel = function () {
     this.experienceUntilNextLevel = this.currentUser.level * 70;
   }
 
-  this.calculateDisplayedExperiencePoints = function() {
-    this.displayedExperiencePoints = this.currentUser.experiencePoints % this.experienceUntilNextLevel;
-  }
-
-  this.calculateUserLevel = function() {
+  this.calculateUserLevel = function () {
     if (this.currentUser.experiencePoints >= this.experienceUntilNextLevel) {
-      this.currentUser.experiencePoints = this.currentUser.experiencePoints % this.experienceUntilNextLevel;
       this.currentUser.level = this.currentUser.level + 1;
+      // Start at the beginning again
+      this.currentUser.experiencePoints = 0;
     }
   }
 
+  this.calculatePercentage = function () {
+    this.percentage = (100 / this.experienceUntilNextLevel) * this.currentUser.experiencePoints;
+    if (this.progressElement) {
+      this.progressElement.set(this.percentage);
+    }
+  }
 });
